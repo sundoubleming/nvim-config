f252f75cacb99774ca1a3529aa6e1c6fb0704266
sunminming <1749005705@qq.com>
2025-08-14 17:43:23 +0800

    Update nvim-cmp

--- lua/init.lua
+++ lua/init.lua
@@ -1,42 +1,36 @@
---require("nvim-treesitter.configs").setup({
-	-- 手动指定解析器路径（关键步骤）
-	--parser_install_dir = "~/.local/share/nvim/treesitter/parsers",
---	ensure_installed = {}, -- 禁用自动安装
---	highlight = { enable = false },
---	indent = { enable = true, disable = { "c", "cpp" } }, -- 禁用 C/C++ 缩进（避免宏导致缩进异常）[6](@ref)
---	incremental_selection = { enable = true }, -- 增量选择
---	fold = { enable = true }, -- 代码折叠
---})
+local cmp = require("cmp")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
+cmp.setup({
+	--completion = {
+	--	autocomplete = false
+	--},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-n>"] = cmp.mapping.select_next_item(), -- 添加导航键
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "buffer", keyword_length = 4 },
+	})
+})
 
 local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr, desc = "Go to Function Definition"})
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {buffer = bufnr, desc = "Go to Function Declaration"})
 end
 
 require'lspconfig'.clangd.setup{
 	on_attach = on_attach,
 	cmd = {"clangd",
-		"--log=verbose",
 		"--background-index",
 		"--all-scopes-completion",
 		"--completion-style=detailed",
 		"--cross-file-rename",
 		"--limit-references=0",
-	}
+	},
+	capabilities = require("cmp_nvim_lsp").default_capabilities()
 }
-
---require("clangd_extensions").setup({
---	server = {  -- 关键：将配置写在 server 字段内
---		on_attach = on_attach,
---		cmd = {"clangd",
---			"--log=verbose",
---			"--background-index",
---			"--all-scopes-completion",
---			"--completion-style=detailed",
---			"--cross-file-rename",
---			"--limit-references=0",
---		}
---	},
---  extensions = { autoSetHints = true }  -- 扩展功能配置
---})
 
